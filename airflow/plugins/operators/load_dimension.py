686d36791bc25a5938d3c1a4004923016fbb57f3
Mark Chang <ak0922747401@gmail.com>
2020-02-10 21:46:09 +0800

    inplement load dim

--- airflow/plugins/operators/load_dimension.py
+++ airflow/plugins/operators/load_dimension.py
@@ -2,21 +2,41 @@ from airflow.hooks.postgres_hook import PostgresHook
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
 
+
 class LoadDimensionOperator(BaseOperator):
 
     ui_color = '#80BD9E'
+
+    insert_sql = """
+    INSERT INTO {target_table}
+    {select_sub_sql}
+    """
 
     @apply_defaults
     def __init__(self,
                  # Define your operators params (with defaults) here
                  # Example:
                  # conn_id = your-connection-name
+                 select_sub_sql="",
+                 target_table="",
+                 redshift_conn_id="",
                  *args, **kwargs):
 
         super(LoadDimensionOperator, self).__init__(*args, **kwargs)
         # Map params here
         # Example:
         # self.conn_id = conn_id
+        self.redshift_conn_id = redshift_conn_id
+        self.target_table = target_table
+        self.select_sub_sql = select_sub_sql
 
     def execute(self, context):
-        self.log.info('LoadDimensionOperator not implemented yet')
+        redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+        self.log.info("Clearing data from destination Redshift table")
+        redshift.run("TRUNCATE {}".format(self.target_table))
+        self.log.info(f"Loading data to dimension table {self.target_table}")
+        formatted_sql = LoadDimensionOperator.insert_sql.format(
+            target_table=self.target_table,
+            select_sub_sql=self.select_sub_sql
+        )
+        redshift.run(formatted_sql)
